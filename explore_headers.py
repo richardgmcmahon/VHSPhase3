1cd22ebfa7ea8101a468024f10dfa1b71ca6b9dd
richardgmcmahon <rgm@ast.cam.ac.uk>
2017-08-09 00:01:40 -0700

    adding Vega system metadata to mag and colour columns 	modified:   explore_headers.py

--- explore_headers.py
+++ explore_headers.py
@@ -2,6 +2,8 @@ from __future__ import print_function, division
 
 """
 
+TODO: need to add Vega system after mag i.e. mag; Vega system
+
 
 FITS file keywords and astropy.table
     http://docs.astropy.org/en/stable/io/unified.html#fits
@@ -32,10 +34,243 @@ dictionary and does not fully represent the structure of a FITS header
 (for example, keyword comments are dropped).
 
 
+see also: http://almaost.jb.man.ac.uk/help/FITSFIXER.py
+
+
 """
 
-
+import numpy as np
 from astropy.io import fits
+
+from astropy.table import Table
+
+
+def mk_phase3_newheader(header=None, debug=True):
+    """
+
+    make a new header with keywords sorted by field/column
+
+    """
+
+    keyword_list_mandatory = ['TTYPE', 'TFORM']
+    keyword_list_optional = ['TCOMM', 'TUNIT', 'TUCD']
+
+    # keyword_list_all = keyword_list_mandatory.append(keyword_list_optional)
+    keyword_list_all = keyword_list_mandatory + keyword_list_optional
+
+    print('keywords:', keyword_list_mandatory)
+    print('keywords:', keyword_list_mandatory + keyword_list_optional)
+
+    newheader = header.copy()
+
+    nfields = header['TFIELDS']
+    for ifield in range (1, nfields + 1):
+
+        print(ifield)
+
+        keyword = 'TTYPE' + str(ifield)
+        itest = -1
+        value = ''
+        try:
+            itest = header.index(keyword)
+        except:
+            pass
+        if itest  > -1:
+            value = header[itest]
+        print(ifield, itest, keyword, value)
+
+        keyword_next = 'TFORM' + str(ifield)
+        itest = header.index(keyword_next)
+        print(itest, type(header[itest]), header[itest])
+        #print(itest, header.keys(itest))
+        print(itest, header.comments[itest])
+        newheader.set(keyword_next, header[itest], after=keyword)
+
+        keyword = keyword_next
+        keyword_next = 'TUNIT' + str(ifield)
+        # locate keyword in original header and copy it
+        print(itest, type(header[itest]), header[itest])
+        itest = header.index(keyword_next)
+        newheader.set(keyword_next, header[itest], after=keyword)
+
+
+        keyword = keyword_next
+        keyword_next = 'TUCD' + str(ifield)
+        # locate keyword in original header and copy it
+        print(itest, type(header[itest]), header[itest])
+        itest = header.index(keyword_next)
+        newheader.set(keyword_next, header[itest], after=keyword)
+
+        keyword = keyword_next
+        keyword_next = 'TCOMM' + str(ifield)
+        # locate keyword in original header and copy it
+        print(itest, type(header[itest]), header[itest])
+        itest = header.index(keyword_next)
+        print(keyword_next, header[itest])
+        # look for mag in the string
+        itest_mag = header[itest].find(' mag ')
+        if itest_mag > -1:
+            print('mag test:', header[itest])
+            print(itest_mag)
+            raw_input("Press ENTER to continue: ")
+
+
+        itest_colour = header[itest].find(' colour')
+        if itest_colour > -1:
+            print('colour test:', header[itest])
+            print(itest_colour)
+            raw_input("Press ENTER to continue: ")
+
+        newheader.set(keyword_next, header[itest], after=keyword)
+
+
+    print(newheader)
+
+    return newheader
+
+
+def fits_header_fix_keyword_order(header=None):
+    """
+
+
+
+    """
+
+    # look through by column number and copy the cards sequentially
+    # create a list of unique keyword prefixes by striping off the field
+    # numbers.
+    # i.e.
+    #    TTYPE1 -> TTYPE
+    #    TTYPE2 -> TTYPE
+    #    TFORM1 -> TFORM
+    #
+    # order within a column is:
+    #    TTYPE
+    #    TFORM
+
+
+    for ifield in range(1, nfields + 1):
+        print(ifield)
+        keyword = 'TTYPE' + str(ifield)
+        itest = -1
+        value = ''
+        try:
+            itest = header.index(keyword)
+        except:
+            pass
+        if itest  > -1:
+            value = header[itest]
+
+
+
+def fits_header_fix_units(header=None):
+    """
+
+
+    """
+    keyword_list_mandatory = ['TTYPE', 'TFORM']
+    keyword_list_optional = ['TCOMM', 'TUNIT', 'TUCD']
+
+    # keyword_list_all = keyword_list_mandatory.append(keyword_list_optional)
+    keyword_list_all = keyword_list_mandatory + keyword_list_optional
+
+    print('keywords:', keyword_list_mandatory)
+    print('keywords:', keyword_list_mandatory + keyword_list_optional)
+
+
+    for ifield in range(1, nfields + 1):
+        print(ifield)
+        keyword = 'TTYPE' + str(ifield)
+        itest = -1
+        value = ''
+        try:
+            itest = header.index(keyword)
+        except:
+            pass
+        if itest  > -1:
+            value = header[itest]
+
+        print(ifield, itest, keyword, value)
+
+
+
+
+    # galact longitude and latitude
+    if TTYPE == 'L' and TUNITS == '':
+         TUNITS = 'deg'
+
+
+    result = header
+
+    return result
+
+
+def fits_column_stats(data=None, ifield=None, debug=True):
+    """
+
+    see also table_stats
+
+    """
+
+    # ifield is 1-indexed; icolumn is 0-indexed
+    icolumn = ifield - 1
+
+    # try to compute stats for numeric columns and then strings
+    # help(data)
+    print()
+    print(ifield, type(data))
+    print(ifield, type(data.field(icolumn)))
+    print(ifield, data.field(icolumn).shape, data.field(icolumn).size)
+    print(ifield, type(data.field(icolumn)[0]))
+
+    data_min = None
+    data_max = None
+
+    n_unique = np.unique(data.field(icolumn))
+
+    if isinstance(data.field(icolumn)[0], basestring):
+        print(ifield, 'This column is a character array')
+
+    try:
+        data_min = np.nanmin(data.field(icolumn))
+    except:
+        data_min= np.min(np.array(data.field(icolumn), dtype=object))
+
+    try:
+        data_max = np.nanmax(data.field(icolumn))
+    except:
+        data_max = np.max(np.array(data.field(icolumn), dtype=object))
+
+    data_range = None
+    try:
+        data_range = data_max - data_min
+    except:
+        pass
+
+    try:
+        print(ifield, data.field(column).shape,
+              data.ifield(column).size, len(data.ifield(icolumn)))
+        print(ifield, data_min, data_max,
+              len(data.field(icolumn)[np.isnan(data.field(icolumn))]))
+        print('Range:', data_range)
+        print(ifield, 'Unique values:', len(n_unique))
+    except:
+        # deal with the strings
+        # http://stackoverflow.com/questions/12654093/arrays-of-strings-into-numpy-amax
+        # data_dtype = data[ifield].dtype
+        # print(ifield, data[ifield].shape, data[ifield].size, data_type)
+        print(ifield, 'Range:', data_min, data_max, data_range)
+        print(ifield, 'Unique values:', len(n_unique))
+
+    result = None
+
+    if ifield < 10 and debug:
+        raw_input("Press ENTER to continue: ")
+
+    return result
+
+
+
 
 def explore_table_header(infile=None, header=None, debug=True):
 
@@ -54,28 +289,172 @@ def explore_table_header(infile=None, header=None, debug=True):
 
 def explore_fits_header(infile=None, header=None, debug=True):
 
-
     # read with astropy.io.fits
     hdulist = fits.open(infile)
+
     # help(fits)
     # help(hdulist)
 
+
+    keyword_list_mandatory = ['TTYPE', 'TFORM']
+    keyword_list_optional = ['TUCD', 'TCOMM', 'TUNIT']
+
+    # keyword_list_all = keyword_list_mandatory.append(keyword_list_optional)
+    keyword_list_all = keyword_list_mandatory + keyword_list_optional
+
+    print('keywords:', keyword_list_mandatory)
+    print('keywords:', keyword_list_mandatory + keyword_list_optional)
+
     hdulist.info()
-    print('type(hdulist):', type(hdulist), len(hdulist))
+    print('type(hdulist):', type(hdulist))
     print('Number of HDUs:', len(hdulist))
     print('hdulist.filename:', hdulist.filename())
 
     header = hdulist[1].header
+    print('type(header):', type(header))
+    print('type(header[0]):', type(header[0]), len(header[0]))
+    print('Number of bytes per row:', header['NAXIS1'])
+    print('Number of table rows:', header['NAXIS2'])
+    print('Number of table fields/columns:', header['TFIELDS'])
+    print()
+    help(header)
+    print('Header items:', header.items())
+    help(header.items)
+    print()
+    print('Header keywords:', header.keys())
+    help(header.keys)
+    print()
+    print('Header values:', header.values())
+    help(header.values)
+    print()
+    # print('Header comments:', header.comments())
+    for key in header.keys():
+        print(key)
+
+    raw_input("Press ENTER to continue: ")
+
+
+    tbdata = hdulist[1].data
+    print('type (tbdata):', type(tbdata))
+    print('len(tbdata):', len(tbdata))
+
+    columns = hdulist[1].columns
+    columns.info()
+
+    if debug: raw_input("Press ENTER to continue: ")
+
+    # copy the header
+    header_copy = header
+    nfields = header['TFIELDS']
+    for ifield in range (1, nfields + 1):
+        print(ifield)
+        keyword = 'TTYPE' + str(ifield)
+        itest = -1
+        value = ''
+        try:
+            itest = header.index(keyword)
+        except:
+            pass
+        if itest  > -1:
+            value = header[itest]
+
+        print(ifield, itest, keyword, value)
+
+
+        keyword = 'TFORM' + str(ifield)
+        itest = -1
+        value = ''
+        try:
+            itest = header.index(keyword)
+        except:
+            pass
+        if itest  > -1:
+            value = header[itest]
+
+        print(ifield, itest, keyword, value)
+
+        keyword = 'TCOMM' + str(ifield)
+        itest = -1
+        value = ''
+        try:
+            itest = header.index(keyword)
+        except:
+            pass
+        if itest  > -1:
+            value = header[itest]
+
+        print(ifield, itest, keyword, value)
+
+
+
+        keyword = 'TUNIT' + str(ifield)
+        itest = -1
+        value = ''
+        try:
+            itest = header.index(keyword)
+        except:
+            pass
+        if itest  > -1:
+            value = header[itest]
+
+        print(ifield, itest, keyword, value)
+
+        keyword = 'TUCD' + str(ifield)
+        itest = -1
+        value = ''
+        try:
+            itest = header.index(keyword)
+        except:
+            pass
+        if itest  > -1:
+            value = header[itest]
+
+        print(ifield, itest, keyword, value)
+
+        # compute column/field stats
+        fits_column_stats(data=tbdata, ifield=ifield)
+
+
+    # determine the list of unique keywords with the prefix T
+
+    if debug: raw_input("Press ENTER to continue: ")
+
+    help(header)
+    # find the keywords for column1
+    itest = -1
+    try:
+        itest = header.index('TTYPE')
+    except:
+        pass
+
+    print(itest)
+    itest = header.index('TTYPE1')
+    print(itest)
+
+
+    if debug: raw_input("Press ENTER to continue: ")
+    help(fits.Header)
+
+    if debug: raw_input("Press ENTER to continue: ")
+
+    # Header.index
+    # Returns the index if the first instance of the given keyword in the
+    # header, similar to `list.index` if the Header object is treated as a
+    # list of keywords.
+    help(fits.Header.index)
 
     print(header)
 
     print('type(header):', type(header), len(header))
     print(type(header[0]), len(header[0]))
     print(header[0], header.comments)
-    print(header[0].cards)
+
+    if debug: raw_input("Press ENTER to continue: ")
+
     # print(header[0].comments)
 
     help(header)
+    # list all the header cards
     for (icard, card) in enumerate(header):
         print(icard, icard + 1, card, header[icard])
 
@@ -93,28 +472,98 @@ def explore_fits_header(infile=None, header=None, debug=True):
 
 
 
-def fix_units(header=None):
-
-
-    # for key in hdu1.keys():
-
-    # galact longitude
-    if TTYPE == 'L' and unit == '':
-        units = 'deg'
-
-
 if __name__ == "__main__":
 
+    import os
+    import sys
+    import time
+
+    import traceback
+    import inspect
+    import logging
+
+    # format = '%(asctime)s.%(msecs)02d %(levelname)s %(name)s' \
+    #         '%(module)s - %(funcName)s: %(message)s'
+
+    format = '%(asctime)s %(levelname)s %(name)s' \
+               '%(module)s - %(funcName)s: %(message)s'
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format=format,
+        datefmt="%Y-%m-%d %H:%M:%S")
+
+    logger = logging.getLogger(__name__)
 
     inpath = '/data/desardata/MachineLearning/VHS_DR4_ESO/'
     filename = 'fs472446409191_v20110814_00914_586174.fits'
 
     infile = inpath + filename
 
-    explore_header_table(infile=infile, header=None)
+    hdulist = fits.open(infile)
+    hdulist.info()
+    header = hdulist[1].header
+    data = hdulist[1].data
 
-    explore_header_fits(infile=infile, header=None)
 
+    print()
+    logger.info('Writing out some info')
+
+    print('hdulist.filename():', hdulist.filename())
+    # print('header.filename():', header.filename())
+    # print('data.filename():', data.filename())
+    print('hdulist.fileinfo(0):', hdulist.fileinfo(0))
+    print('hdulist.fileinfo(1):', hdulist.fileinfo(1))
+    print('hdulist[0].header[0]:', hdulist[0].header[0])
+    print('hdulist[0].header[1]:', hdulist[0].header[1])
+    print('hdulist[0].header.cards[0]:', hdulist[0].header.cards[0])
+    print('hdulist[0].header.cards[0][0]:', hdulist[0].header.cards[0][0])
+    print('hdulist[0].header.cards[0][1]:', hdulist[0].header.cards[0][1])
+    print('hdulist[0].header.cards[0][2]:', hdulist[0].header.cards[0][2])
+    # print('header.fileinfo():', header.fileinfo(0))
+    print('CHECKSUM (original value):', header['CHECKSUM'])
+    print('DATASUM (original value):', header['DATASUM'])
+
+    # fix phase3 header
+    newheader = mk_phase3_newheader(header=header, debug=True)
+
+    print()
+    logger.info('Writing out results')
+    print('hdulist.filename():', hdulist.filename())
+    print('hdulist[0].header[0]:', hdulist[0].header[0])
+    print('hdulist[0].header[1]:', hdulist[0].header[1])
+    print('hdulist[0].header.cards[0]:', hdulist[0].header.cards[0])
+    print('hdulist[0].header.cards[0][0]:', hdulist[0].header.cards[0][0])
+    print('hdulist[0].header.cards[0][1]:', hdulist[0].header.cards[0][1])
+    print('hdulist[0].header.cards[0][2]:', hdulist[0].header.cards[0][2])
+    # print('header.fileinfo():', header.fileinfo(0))
+    # print('header.filename():', header.filename())
+    # print('data.filename():', data.filename())
+    print('hdulist.fileinfo(0):', hdulist.fileinfo(0))
+    print('hdulist.fileinfo(1):', hdulist.fileinfo(1))
+    print('CHECKSUM (original value):', header['CHECKSUM'])
+    print('DATASUM (original value):', header['DATASUM'])
+    hdulist[1].header = newheader
+
+    hdulist.writeto('tmp.fits', overwrite=True, checksum=True)
+
+    # read back check
+    hdulist = fits.open('tmp.fits')
+    header = hdulist[1].header
+    print('hdulist.filename():', hdulist.filename())
+    print('CHECKSUM (new value):', header['CHECKSUM'])
+    print('DATASUM (new value):', header['DATASUM'])
+    raw_input("Press ENTER to continue: ")
+
+    # read astropy.Table
+    table = Table.read('tmp.fits')
+    # read astropy.Table
+    table.meta['filename'] = 'tmp.fits'
+    print("table.meta['filename']:", table.meta['filename'])
+
+    explore_fits_header(infile=infile, header=None)
+
+    explore_table_header(infile=infile, header=None)
 
     # read astropy.Table
     table = Table.read(infile)
